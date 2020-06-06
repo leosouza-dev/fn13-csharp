1545c9a24c63d34276353084776937e7453d5865
sarival <sarival@sarival.com.br>
2020-06-06 11:49:07 -0300

    Add project files.

---
+++ OlaMundo/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace OlaMundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_OlaMundo = new System.Windows.Forms.Button();
+            this.Btn_PrimeiraConta = new System.Windows.Forms.Button();
+            this.btn_conv = new System.Windows.Forms.Button();
+            this.Btn_controle = new System.Windows.Forms.Button();
+            this.btn_repeticao = new System.Windows.Forms.Button();
+            this.btn_for = new System.Windows.Forms.Button();
+            this.btn_rendimento = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_OlaMundo
+            // 
+            this.Btn_OlaMundo.Location = new System.Drawing.Point(12, 12);
+            this.Btn_OlaMundo.Name = "Btn_OlaMundo";
+            this.Btn_OlaMundo.Size = new System.Drawing.Size(114, 23);
+            this.Btn_OlaMundo.TabIndex = 0;
+            this.Btn_OlaMundo.Text = "Olá Mundo";
+            this.Btn_OlaMundo.UseVisualStyleBackColor = true;
+            this.Btn_OlaMundo.Click += new System.EventHandler(this.Btn_OlaMundo_Click);
+            // 
+            // Btn_PrimeiraConta
+            // 
+            this.Btn_PrimeiraConta.Location = new System.Drawing.Point(12, 41);
+            this.Btn_PrimeiraConta.Name = "Btn_PrimeiraConta";
+            this.Btn_PrimeiraConta.Size = new System.Drawing.Size(114, 23);
+            this.Btn_PrimeiraConta.TabIndex = 1;
+            this.Btn_PrimeiraConta.Text = "Primeira Conta";
+            this.Btn_PrimeiraConta.UseVisualStyleBackColor = true;
+            this.Btn_PrimeiraConta.Click += new System.EventHandler(this.Btn_PrimeiraConta_Click);
+            // 
+            // btn_conv
+            // 
+            this.btn_conv.Location = new System.Drawing.Point(12, 70);
+            this.btn_conv.Name = "btn_conv";
+            this.btn_conv.Size = new System.Drawing.Size(114, 23);
+            this.btn_conv.TabIndex = 2;
+            this.btn_conv.Text = "Conversão";
+            this.btn_conv.UseVisualStyleBackColor = true;
+            this.btn_conv.Click += new System.EventHandler(this.btn_conv_Click);
+            // 
+            // Btn_controle
+            // 
+            this.Btn_controle.Location = new System.Drawing.Point(12, 99);
+            this.Btn_controle.Name = "Btn_controle";
+            this.Btn_controle.Size = new System.Drawing.Size(114, 23);
+            this.Btn_controle.TabIndex = 3;
+            this.Btn_controle.Text = "Estrutura de Controle";
+            this.Btn_controle.UseVisualStyleBackColor = true;
+            this.Btn_controle.Click += new System.EventHandler(this.Btn_controle_Click);
+            // 
+            // btn_repeticao
+            // 
+            this.btn_repeticao.Location = new System.Drawing.Point(12, 128);
+            this.btn_repeticao.Name = "btn_repeticao";
+            this.btn_repeticao.Size = new System.Drawing.Size(139, 23);
+            this.btn_repeticao.TabIndex = 4;
+            this.btn_repeticao.Text = "Estrutura de Repeticao";
+            this.btn_repeticao.UseVisualStyleBackColor = true;
+            this.btn_repeticao.Click += new System.EventHandler(this.btn_repeticao_Click);
+            // 
+            // btn_for
+            // 
+            this.btn_for.Location = new System.Drawing.Point(12, 157);
+            this.btn_for.Name = "btn_for";
+            this.btn_for.Size = new System.Drawing.Size(139, 23);
+            this.btn_for.TabIndex = 5;
+            this.btn_for.Text = "For";
+            this.btn_for.UseVisualStyleBackColor = true;
+            this.btn_for.Click += new System.EventHandler(this.btn_for_Click);
+            // 
+            // btn_rendimento
+            // 
+            this.btn_rendimento.Location = new System.Drawing.Point(12, 186);
+            this.btn_rendimento.Name = "btn_rendimento";
+            this.btn_rendimento.Size = new System.Drawing.Size(139, 23);
+            this.btn_rendimento.TabIndex = 6;
+            this.btn_rendimento.Text = "Calc. Rendimento";
+            this.btn_rendimento.UseVisualStyleBackColor = true;
+            this.btn_rendimento.Click += new System.EventHandler(this.btn_rendimento_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 235);
+            this.Controls.Add(this.btn_rendimento);
+            this.Controls.Add(this.btn_for);
+            this.Controls.Add(this.btn_repeticao);
+            this.Controls.Add(this.Btn_controle);
+            this.Controls.Add(this.btn_conv);
+            this.Controls.Add(this.Btn_PrimeiraConta);
+            this.Controls.Add(this.Btn_OlaMundo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_OlaMundo;
+        private System.Windows.Forms.Button Btn_PrimeiraConta;
+        private System.Windows.Forms.Button btn_conv;
+        private System.Windows.Forms.Button Btn_controle;
+        private System.Windows.Forms.Button btn_repeticao;
+        private System.Windows.Forms.Button btn_for;
+        private System.Windows.Forms.Button btn_rendimento;
+    }
+}
+
---
+++ OlaMundo/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OlaMundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_OlaMundo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Olá Mundo");
+        }
+
+        private void Btn_PrimeiraConta_Click(object sender, EventArgs e)
+        {
+            // tipo + nomeDaVariavel
+            string nome = "Caio";
+            string sobrenome = "Silva";
+            int numero = 1;
+            double saldo = 100.0;
+            double valorSaque = 10;
+
+            // sacar 10
+            //saldo = saldo - valorSaque;
+            saldo -= valorSaque;
+
+            MessageBox.Show("O nome do titular: " + nome + " " +  sobrenome);
+
+            MessageBox.Show("O numero da conta: " + numero);
+            MessageBox.Show("O saldo é : " + saldo);
+        }
+
+        private void btn_conv_Click(object sender, EventArgs e)
+        {
+            int valorInt = 100;
+
+            short valorShort;
+            long valorLong;
+
+            //passar o int para long
+            valorLong = valorInt;
+
+            //passar o int para short - casting
+            valorShort = (short)valorInt;
+
+        }
+
+        private void Btn_controle_Click(object sender, EventArgs e)
+        {
+            int numero = 1;
+            double saldo = 100.0;
+            double valorSaque = -10;
+
+            // true ou false
+            // > (maior), == (igual), <, <=
+            // OU ||
+            // And &&
+            // !
+            bool condicaoSaque = saldo >= valorSaque && valorSaque > 0;
+
+            if (condicaoSaque)
+            {
+                saldo -= valorSaque; 
+                MessageBox.Show("Saque realizado");
+            }
+            else
+            {
+                MessageBox.Show("Saque NÃO Realizado");
+            }
+
+            MessageBox.Show("" + saldo);
+
+
+        }
+
+        private void btn_repeticao_Click(object sender, EventArgs e)
+        {
+            // do while, for, while, foreach
+
+            //while
+            int i = 11; // inicialização
+
+            do
+            {
+                MessageBox.Show("C# é melhor que Java");
+            } while (i < 10);
+
+            //while (i < 10) // condição
+            //{
+            //    MessageBox.Show("C# é melhor que Java");
+            //    //i = i + 1;
+            //    //i += 1;
+            //    i++; // incremento
+            //}
+
+        }
+
+        private void btn_for_Click(object sender, EventArgs e)
+        {
+            // for
+            for (int i = 0; i < 10; i++)
+            {
+                MessageBox.Show("C# é melhor que Java");
+            }
+
+        }
+
+        private void btn_rendimento_Click(object sender, EventArgs e)
+        {
+            // calculo de rendimento após 1 mês
+            double valorInvestido = 1000.0;
+
+            //calculo de rendimento
+            //valorInvestido = valorInvestido + valorInvestido * 0.01;
+
+            for (int i = 0; i < 12; i++)
+            {
+                valorInvestido += valorInvestido * 0.01;
+            }
+
+            MessageBox.Show("Valor Investido após um ano: " + valorInvestido.ToString("N2"));
+
+        }
+    }
+}
